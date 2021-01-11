a161e4ec62d209ab4bb897cdae9d723f1a29b661
htoupe <htoupe1@gmail.com>
2021-01-11 00:36:50 -0500

    surefire test

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	
+	@Test
+	public void iosApp()
+	{
+		System.out.println("iosApp");
+	}
+
+}
